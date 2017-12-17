1ab344c08a681976345d6718985de0a69b299cf1
Justin Wu <juns3700@gmail.com>
2017-12-17 22:40:44 +0800

    add city.js

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const city = require('./city');
 const url = 'http://www.cwb.gov.tw/V7/forecast/week/week.htm';
 
 request(url, (err, res, body) => {
-    const $ = cheerio.load(body);
+    console.log(city.citys);
+    //const $ = cheerio.load(body);
     //const cityId = [];
-    let weathers = [];
+    //let weathers = [];
     //console.log("data",city.getAllCityNames());
     //抓白天天氣
-    $('.BoxTableInside tbody tr').eq(1).find('img').each(function(i, elem) {
-        weathers.push($(this).attr('src'));
-      })
-     //抓晚上天氣
-    $('.BoxTableInside tbody tr').eq(2).find('img').each(function(i, elem) {
-        weathers.push($(this).attr('src'));
-      })
-    console.log(weathers);
+    // $('.BoxTableInside tbody tr').eq(1).find('img').each(function(i, elem) {
+    //     weathers.push($(this).attr('src'));
+    //   })
+    //  //抓晚上天氣
+    // $('.BoxTableInside tbody tr').eq(2).find('img').each(function(i, elem) {
+    //     weathers.push($(this).attr('src'));
+    //   })
+    // console.log(weathers);
     //console.log(weathers[49][3]);
 })
